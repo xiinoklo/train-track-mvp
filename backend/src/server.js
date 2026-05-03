2bc21464ec18d1659a1e6ca5540edda4c2cbda2f
JoakoDeAstora <joaquin1troncoso@gmail.com>
2026-05-03 18:08:42 -0400

    Mejora en ciberseguridad, quitamos inyection noSQL y payload infinitos

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,20 +1,24 @@
 const express = require("express");
 const cors = require("cors");
-const connectDB = require("./config/db"); // Importa la conexión
+const mongoSanitize = require("express-mongo-sanitize");
+const connectDB = require("./config/db");
 
 const authRoutes = require("./routes/authRoutes");
 const exerciseRoutes = require("./routes/exerciseRoutes");
 const workoutRoutes = require("./routes/workoutRoutes");
 const wellnessRoutes = require("./routes/wellnessRoutes");
 
-// Conectar a Mongoose inmediatamente
 connectDB();
 
 const app = express();
-const PORT = process.env.PORT || 3000; // Usa el puerto del .env
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
-app.use(express.json());
+
+app.use(express.json({ limit: "10kb" }));
+
+app.use(mongoSanitize());
+
 app.use("/api/auth", authRoutes);
 
 app.get("/health", (req, res) => {
